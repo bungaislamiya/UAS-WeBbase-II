3c6dcfb7f14465795398e3c7917a5b929225c303
Bunga Islamiya Putri <97377281+bungaislamiya@users.noreply.github.com>
2022-01-11 17:45:10 +0700

    Hakaman Utama (Welcome)

---
+++ welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="/css/template.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <title>ImParfumKu | Welcome</title>
+    </head>
+
+    <body id="bg">
+        <div id="home">
+                <nav style ="padding : 20px;" >
+                    <ul id="navigasi">
+                        <li><a href="/" class="font" style= "margin-left :65rem;">Home</a></li>
+                        <li><a href="/about" class="font">About</a></li>
+                        <li class="dropdown"><a href="#" class="font">Content</a>
+                            <ul class="isi-dropdown">
+                                <li><a href="/woman" class="font1"><img src="/img/woman.png" width="30" height="30" style="margin-left : 10px;"/>Aroma Woman</a></li>
+                                <li><a href="/man" class="font1"><img src="/img/man.png" width="30" height="30" style="margin-left : 10px;"/>Aroma Man</a></li>
+                                
+                        </li>
+                    </ul>
+                </nav>
+        </br>
+        </br>
+
+        </div>
+<center>
+       <div class="mt-2">
+            
+            <h1 class ="art"> Resseler Resmi IMParfum <br> Bunga Islamiya Putri</h1>
+            
+        </div>
+        </center>
+    </body>
+
+</html>
