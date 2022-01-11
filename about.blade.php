2c58bd1fb4d3ce38b6aa38ed0bfe7aeb88dfba6e
Bunga Islamiya Putri <97377281+bungaislamiya@users.noreply.github.com>
2022-01-11 17:44:16 +0700

    Halaman about

---
+++ about.blade.php
@@ -0,0 +1,39 @@
+@extends('template')
+@section('title','ImParfumKu | About')
+
+@section('isi')
+<div>
+    <ul>
+        <li>
+            <img src="/img/parfum24.jpg" width="480" height="850">  
+             
+        </li>
+
+        <li style="padding : 1rem;"> 
+            <p class="fon"> Sejarah IMParfume</p>
+            <p class="font">Parfum sudah dikenal sejak ribuan tahun yang lalu. 
+                Kata “parfum” berasal dari bahasa Latin ”per fume” artinya “melalui asap”.
+                Salah satu kegunaan parfum tertua berupa bentuk pembakaran dupa dan herbal 
+                aromatik yang digunakan dalam pelayanan keagamaan, seringkali untuk aromatik gums, 
+                kemenyan dan mur, dikumpulkan dari pohon. Mesir adalah yang pertama memasukkan parfum 
+                ke budaya mereka diikuti oleh Cina kuno, Hindu, Israel, Carthaginians, Arab, Yunani, dan 
+                Romawi. Penggunaan awal dari botol parfum adalah di Mesir sekitar 1000 SM. Mesir menemukan 
+                gelas dan botol parfum adalah salah satu penggunaan umum pertama untuk kaca.</p>
+            <p class="fonn">Tentang IM PARFUME</p>
+            <p class="font">IM Parfume (Indonesia Maju Parfume) IM Parfum 35 ml (cc) merupakan produk Parfum 
+                Import yang bibit nya di import dari negara Singapore dan diracik oleh anak bangsa dan memiliki 
+                1620 lebih aroma yang dapat anda pilih dengan komposisi 75 % bibit dan 25 % absolut. 
+                Dibuat dengan kualitas yang sangat baik dan harga yang sangat terjangkau serta tidak berbekas di baju.</p>
+            <p class="fonnn">KEUNGGULAN IM PARFUME</p>
+            <p class="font">1. Harga Murah.<br>
+                            2. Wangi Tahan Lama, dan (bukan refill/isi ulang).<br>
+                            3. Wanginya menyerupai Wangi Parfum Branded (yang mahal harganya).<br>
+                            4. Banyak variasi/jenis wangi-wangiannya.<br>
+                            5. Komposisi bibit 75% dan absolut 25%.<br>
+                            6. Memiliki 1620 varian aroma.</p>
+        </li>    
+
+    </ul>
+</div>
+
+@endsection
