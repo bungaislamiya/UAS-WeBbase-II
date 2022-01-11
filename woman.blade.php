fb437035a86136d0119dfc633baa1a8678e0bca1
Bunga Islamiya Putri <97377281+bungaislamiya@users.noreply.github.com>
2022-01-11 17:46:39 +0700

    Halaman Daftar Aroma Woman

---
+++ woman.blade.php
@@ -0,0 +1,42 @@
+@extends('template')
+@section('title','Imparfumku | Daftar Aroma Woman')
+
+@section('isi')
+
+<center>
+<div style="color: white;">
+<div class="mt-2">
+            <ul style ="margin : 5rem;">
+                <li>
+                    <a href="/Aroma" class="fontmenu">
+                        <div>
+                            <img src="/img/Aroma1.jpeg" width="150" height="400">
+                            <p class="aroma">Lovely</br></p>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/Aroma" class="fontmenu">
+                        <div>
+                            <img src="/img/Aroma2.jpeg" width="150" height="400">
+                            <p class="aroma">Gucci Flora</br></p>
+                        </div>
+                    </a>
+                </li>    
+
+                <li>
+                    <a href="/Aroma" class="fontmenu">
+                        <div>
+                            <img src="/img/Aroma3.jpeg" width="150" height="400">
+                            <p class="aroma">Diamor Woman</br></p>
+                        </div>
+                    </a>
+                </li>  
+
+            </ul>
+        </div>
+</div>
+</center>
+
+@endsection
