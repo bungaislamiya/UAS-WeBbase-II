780993db5139ccb7f32c28f0914b85a1e3136b64
Bunga Islamiya Putri <97377281+bungaislamiya@users.noreply.github.com>
2022-01-11 17:46:15 +0700

    Halaman Daftar Aroma Man

---
+++ man.blade.php
@@ -0,0 +1,43 @@
+@extends('template')
+@section('title','Imparfumku | Daftar Aroma Man')
+
+@section('isi')
+
+<center>
+<div style="color: white;">
+<div class="mt-5">
+            <ul style ="margin : 2rem;">
+                <li>
+                    <a href="/kalori" class="fontmenu">
+                        <div>
+                            <img src="/img/Aroma4.jpeg" width="150" height="400">
+                            <p class="aroma">Jio Platinum</br></p>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/kalori" class="fontmenu">
+                        <div>
+                            <img src="/img/Aroma5.jpeg" width="150" height="400">
+                            <p class="aroma">Ferray Light</br></p>
+                        </div>
+                    </a>
+                </li>    
+
+                <li>
+                    <a href="/kalori" class="fontmenu">
+                        <div>
+                            <img src="/img/Aroma6.jpeg" width="150" height="400">
+                            <p class="aroma">Buberry For Her</br></p>
+                        </div>
+                    </a>
+                </li>  
+                <br/>  
+
+            </ul>
+        </div>
+</div>
+</center>
+
+@endsection
